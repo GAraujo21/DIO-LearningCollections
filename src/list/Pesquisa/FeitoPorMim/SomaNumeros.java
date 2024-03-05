0d2f7069a17eb9b4451cd0e87bd044a6ffe2c538
Gabriel Araujo <gabrielimaaraujo@gmail.com>
2024-03-05 18:16:22 -0300

    Practicing 'Pesquisa'

--- src/list/Pesquisa/FeitoPorMim/SomaNumeros.java
+++ src/list/Pesquisa/FeitoPorMim/SomaNumeros.java
@@ -67,10 +67,36 @@ public class SomaNumeros {
     }
 
     public void exibirNumeros(){
-        if(listaNumeros.isEmpty()){
+        if(!listaNumeros.isEmpty()){
             System.out.println(this.listaNumeros);
         }else{
             System.out.println("A lista está vazia");
         }
+    }
+
+
+    public static void main(String[] args) {
+        // Criando uma instância da classe SomaNumeros
+        SomaNumeros somaNumeros = new SomaNumeros();
+
+        // Adicionando números à lista
+        somaNumeros.adicionarNumero(5);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(0);
+        somaNumeros.adicionarNumero(-2);
+        somaNumeros.adicionarNumero(10);
+
+        // Exibindo a lista de números adicionados
+        System.out.println("Números adicionados:");
+        somaNumeros.exibirNumeros();
+
+        // Calculando e exibindo a soma dos números na lista
+        System.out.println("Soma dos números = " + somaNumeros.calcularSoma());
+
+        // Encontrando e exibindo o maior número na lista
+        System.out.println("Maior número = " + somaNumeros.encontrarMaiorNumero());
+
+        // Encontrando e exibindo o menor número na lista
+        System.out.println("Menor número = " + somaNumeros.encontrarMenorNumero());
     }
 }
